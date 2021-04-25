5fecf2993aac93be46380679de905a65b498d6c3
maratmuginov <mmuginov@protonmail.com>
2021-04-25 11:23:59 -0700

    Ensure single instance of application

--- source/MakeTopmost/MakeTopmost.Client/Program.cs
+++ source/MakeTopmost/MakeTopmost.Client/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Drawing;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace MakeTopmost.Client
@@ -15,12 +16,25 @@ namespace MakeTopmost.Client
         [STAThread]
         private static void Main()
         {
+            var mutex = new Mutex(false, "e4c6073f-c97b-403c-abf3-95c7e56acc86", out bool createdNew);
+
+            if (createdNew is false)
+                return;
+
+            GC.KeepAlive(mutex);
+            RunApplication();
+        }
+
+        private static void RunApplication()
+        {
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
             var services = ConfigureServices();
             var shellForm = services.GetRequiredService<ShellForm>();
             shellForm.Hide();
+
             Application.Run();
         }
 
